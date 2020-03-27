4b444476b50a7533a8d88b480efe94cfb6354ebc
paulness <paulness@artnet.com>
2020-03-27 16:23:15 -0400

    wip

--- .circleci/install-minikube.sh
+++ .circleci/install-minikube.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-MINIKUBE_VERSION=1.9.0
-curl -Lo minikube \
-    "https://github.com/kubernetes/minikube/releases/download/v{MINIKUBE_VERSION}/minikube-linux-amd64"
-chmod +x minikube
-#sudo mv minikube /usr/local/bin/
-sudo mkdir -p /usr/local/bin/
-sudo install minikube /usr/local/bin/
+# Install the latest Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+   && sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
+# Start minikube
+sudo minikube start minikube start --vm-driver=none --kubernetes-version v1.14.0
 
-minikube start minikube start --vm-driver=none --kubernetes-version v1.14.0
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Install helm
+EXE_FOLDER=/usr/local/bin
+curl -sLo_ https://get.helm.sh/helm-v3.0.2-linux-amd64.tar.gz
+tar -zxvf _
+sudo mv ./linux-amd64/helm ${EXE_FOLDER}/
+sudo chmod +x $EXE_FOLDER/helm
+rm _
+rm -rf linux-amd64
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+
